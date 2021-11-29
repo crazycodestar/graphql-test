557b6ffd1802010002cd8aa01f19ff7b77909246
olalekan <olamilekanadekanmbi@gmail.com>
2021-11-29 22:58:53 +0100

    added mongodb database to backend

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require("graphql");
-const _ = require("lodash");
+// const _ = require("lodash");
+const Author = require("../models/author");
+const Book = require("../models/book");
 
 const {
 	GraphQLObjectType,
@@ -7,18 +9,23 @@ const {
 	GraphQLSchema,
 	GraphQLID,
 	GraphQLInt,
+	GraphQLList,
+	GraphQLNonNull,
 } = graphql;
 
 // dummy data
 const books = [
-	{ name: "Name of winds", genre: "Fantasy", id: "1" },
-	{ name: "The final Empire", genre: "Fantasy", id: "2" },
-	{ name: "The long earth", genre: "Sci-Fi", id: "3" },
+	{ name: "Name of winds", genre: "Fantasy", id: "1", authorId: "1" },
+	{ name: "The final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+	{ name: "The long earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+	{ name: "The Hero of ages", genre: "Fantasy", id: "3", authorId: "2" },
+	{ name: "The Colour of magic", genre: "Fantasy", id: "3", authorId: "3" },
+	{ name: "The Light Fantastic", genre: "Fantasy", id: "3", authorId: "3" },
 ];
 
 const authors = [
 	{ name: "Olalekan Adekanmbi", age: 18, id: "1" },
-	{ name: "Abundance nosurname", age: 16, id: "2" },
+	{ name: "Abundance Anyanwu", age: 16, id: "2" },
 	{ name: "Natheniel anothernoSurname", age: 19, id: "3" },
 ];
 
@@ -28,6 +35,13 @@ const BookType = new GraphQLObjectType({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString },
+		author: {
+			type: AuthorType,
+			resolve(parent, args) {
+				// return _.find(authors, { id: parent.authorId });
+				Author.findById(parent.authorId);
+			},
+		},
 	}),
 });
 
@@ -36,7 +50,14 @@ const AuthorType = new GraphQLObjectType({
 	fields: () => ({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
-		age: { type: graphql.GraphQLInt },
+		age: { type: GraphQLInt },
+		books: {
+			type: new GraphQLList(BookType),
+			resolve(parent, args) {
+				// return _.filter(books, { authorId: parent.id });
+				return Book.find({ authorId: parent.id });
+			},
+		},
 	}),
 });
 
@@ -47,14 +68,52 @@ const RootQuery = new GraphQLObjectType({
 			type: BookType,
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
-				return _.find(books, { id: args.id });
+				// return _.find(books, { id: args.id });
+				return Book.findById(args.id);
 			},
 		},
 		author: {
 			type: AuthorType,
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
-				return _.find(authors, { id: args.id });
+				// return _.find(authors, { id: args.id });
+				return Author.findById(args.id);
+			},
+		},
+	}),
+});
+
+const mutation = new GraphQLObjectType({
+	name: "mutation",
+	fields: () => ({
+		addBooks: {
+			type: BookType,
+			args: {
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				genre: { type: new GraphQLNonNull(GraphQLString) },
+				authorId: { type: new GraphQLNonNull(GraphQLID) },
+			},
+			resolve(parent, args) {
+				const book = new Book({
+					name: args.name,
+					genre: args.genre,
+					authorId: args.authorId,
+				});
+				return book.save();
+			},
+		},
+		addAuthor: {
+			type: AuthorType,
+			args: {
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) },
+			},
+			resolve(parent, args) {
+				const author = new Author({
+					name: args.name,
+					age: args.age,
+				});
+				return author.save();
 			},
 		},
 	}),
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
 
 const schema = require("./schema/schema");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+	useNewUrlParser: true,
+});
 
 app.use(
 	"/graphql",
